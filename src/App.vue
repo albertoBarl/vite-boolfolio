f3d532bd5ec4d9b15a730fa54f1119e713279a93
albertoBarl <alberto.barletta00@gmail.com>
2023-03-15 00:30:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+  <h1>HELLO</h1>
+</template>
+
+<style lang="scss">
+@use "./style/generals.scss" as *;
+@use "./style/partials/mixins" as *;
+@use "./style/partials/variables" as *;
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+
+createApp(App).mount("#app");
