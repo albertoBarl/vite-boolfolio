af06ff5eebaeafda0091ccab2091396768376fa2
albertoBarl <alberto.barletta00@gmail.com>
2023-03-15 01:45:09 +0100

    added content and showed in dom

--- src/components/ProjectMain.vue
+++ src/components/ProjectMain.vue
@@ -1,9 +1,34 @@
 <template lang="">
-  <div></div>
+  <div class="container d-flex flex-wrap gap-3" id="projectMain">
+    <div v-if="loading" class="lds-facebook">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <div
+      v-else
+      class="card"
+      style="max-width: 200px; min-height: 250px"
+      v-for="item in projects"
+      :key="item.id"
+    >
+      <div class="card-img-top">
+        <img
+          :src="`${baseUrl}/storage/${item.cover_image}`"
+          :alt="item.title + '<- image non available'"
+        />
+      </div>
+      <div class="card-title">{{ item.title }}</div>
+      <a href="#" class="btn btn-success">Visualizza dettaglio</a>
+    </div>
+  </div>
 </template>
 
 <!-- script -->
 <script>
+import axios from "axios";
+
 export default {
   name: "ProjectMain",
   data() {
@@ -20,13 +45,16 @@ export default {
       //   axios call to recover API from back-end
       axios.get(`${this.baseUrl}/api/projects`).then((response) => {
         if (response.data.success) {
-          this.project = response.data.results;
+          this.projects = response.data.results;
           this.loading = false;
         } else {
           //   alert for a bad axios call
         }
       });
     },
+  },
+  mounted() {
+    this.getProjects();
   },
 };
 </script>
